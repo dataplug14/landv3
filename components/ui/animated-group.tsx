f1b2efc419467639a78684d1d663e6fa10f6ed83
Oluwamayowa Oladosu <mayowaoladosu2222@gmail.com>
2025-11-08 07:25:43 +0100

    Fix motion proxy typing for animated group

--- components/ui/animated-group.tsx
+++ components/ui/animated-group.tsx
@@ -100,6 +100,9 @@ const addDefaultVariants = (variants: Variants) => ({
   visible: { ...defaultItemVariants.visible, ...variants.visible },
 });
 
+const createMotionProxy = (component: React.ElementType) =>
+  motion.create(component as string | React.ComponentType<unknown>);
+
 function AnimatedGroup({
   children,
   className,
@@ -115,14 +118,8 @@ function AnimatedGroup({
   const containerVariants = variants?.container || selectedVariants.container;
   const itemVariants = variants?.item || selectedVariants.item;
 
-  const MotionComponent = React.useMemo(
-    () => motion.create(as as keyof JSX.IntrinsicElements),
-    [as]
-  );
-  const MotionChild = React.useMemo(
-    () => motion.create(asChild as keyof JSX.IntrinsicElements),
-    [asChild]
-  );
+  const MotionComponent = React.useMemo(() => createMotionProxy(as), [as]);
+  const MotionChild = React.useMemo(() => createMotionProxy(asChild), [asChild]);
 
   return (
     <MotionComponent
